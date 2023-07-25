8747248de713cf172a1aa937dd4cb8a4957ad7ab
Bartosz Lachowicz <jendrekpb@gmail.com>
2023-07-25 12:16:14 +0200

    Refactor chess_utils and add more tests

--- src/chess_utils.py
+++ src/chess_utils.py
@@ -1,61 +1,123 @@
-from collections import deque
-from typing import Deque
+import os
+from typing import List
+import csv
+from PIL import Image
 
+# Chess board states
 E = "E"
 W = "W"
 B = "B"
 
 
-def translate_fe(fe_notation: str):
+class Board:
+    def __init__(self, board: List[int]):
+        self.board = board
+
+    def to_str(self):
+        string_representation = ''
+        for row in self.board:
+            string_representation = f'{string_representation}-{row}'
+        return string_representation
+
+
+def translate_fe_notation(fe_notation: str) -> str:
     """
-    Translates Forysyth-Edwards notation to simple 8x8 chess board disregarding the chess piece type.
-      Only the color and the position of the chess piece is kept resulting in 3 labels:
+    Translates Forysyth-Edwards notation where chess type is disregarded - WBE notation.
 
-        - E for an empty field
-        - W for a white piece
-        - B for a black piece
-
-    As an example let's take 4x4 chess board with a black King in top right corner and a white Rook
-      in the opposite corner:
-
-    Visualized FE notation 3k-4-4-R3':
-
-        [E, E, E, k]
-        [E, E, E, E]
-        [E, E, E, E]
-        [R, E, E, E]
-
-    Translated chess board:
-
-        [E, E, E, B]
-        [E, E, E, E]
-        [E, E, E, E]
-        [W, E, E, E]
-
-    More on the notation: https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
+     Any white piece will be replaced with "W" and any black piece will be replaced with "B", an empty field will be
+     replaced with "E".
     """
+    WBE_notation = ''
+    for row in fe_notation.split('-'):
+        for el in row:
+            if el.isdigit():
+                WBE_notation = f'{WBE_notation}{"E" * int(el)}'
+            if el.isupper():
+                WBE_notation = f'{WBE_notation}W'
+            elif el.islower():
+                WBE_notation = f'{WBE_notation}B'
 
-    simple_chess_board = []
-    rows = fe_notation.split("-")
-    for fe_row in rows:
-        simple_chess_board.append(_translate_fe_row(fe_row))
+        WBE_notation = f'{WBE_notation}-'
 
-    return simple_chess_board
+    return WBE_notation[:-1]  # get rid of the last "-"
 
 
-def _translate_fe_row(FE_row: str):
-    converted_row: Deque[str]= deque()
+def translate_fe_to_int_notation(WBE_notation: str) -> List[int]:
+    """Replace W with 1 B with -1 and E with 0"""
+    int_notation = []
+    for row in WBE_notation.split('-'):
+        for el in row:
+            if el == 'W':
+                int_notation.append(1)
+            elif el == 'B':
+                int_notation.append(-1)
+            else:
+                int_notation.append(0)
 
-    for id in range(0, len(FE_row)):
-        el = FE_row[id]
-        if el.isdigit():
-            for _ in range(0, int(el)):
-                converted_row.append(E)
+    return int_notation
 
-        elif el.isupper():
-            converted_row.append(W)
 
-        elif el.islower():
-            converted_row.append(B)
+def one_hot_wbe(int_notation):
+    """For neural network input of 8x8 board we have 64 inputs, each of them is a single chessboard tile with either of the values - 'W"(1), 'E'(0), 'B'(-1)
+    This function converts the 64 input numbers 192 neuron representation where:
+     - first 64 neurons represent one hot encoded white pieces
+     - next 64 neurons represent one hot encoded 'empty' places
+     - last 64 neurons represent one hot encoded black pieces
+    """
+    white = []
+    empty = []
+    black = []
+    for id in range(len(int_notation)):
+        if int_notation[id] == 1:
+            white.append(1)
+            empty.append(0)
+            black.append(0)
+        elif int_notation[id] == -1:
+            white.append(0)
+            empty.append(0)
+            black.append(1)
+        else:
+            white.append(0)
+            empty.append(1)
+            black.append(0)
 
-    return converted_row
+    for w, e, b in zip(white, empty, black):
+        assert w + e + b == 1, f"WBE encoidng failed, w: {w}, e: {e}, b: {b}"
+    return white, empty, black
+
+# img_names_to_csv(img_names)
+
+# for filename in os.listdir(small_data_dir):
+#     fe_label = os.path.splitext(filename)[0]
+#     print(fe_label)
+#     label = board_to_str(translate_fe(fe_label))
+#     label_dir = f'small_data/labels/{label}'
+#     # print(f'label {label_dir}')
+
+
+#     try:
+#         os.makedirs(label_dir)
+#     except FileExistsError:
+#         continue
+
+# parent_dir = '/Users/my_Username/Desktop/'
+# old_name = 'foo.txt'
+# new_name = 'bar.txt'
+
+# with os.open(parent_dir, os.O_RDONLY) as fd:
+#     os.replace(old_name, new_name, src_dir_fd=fd)
+
+
+    # finally:
+    #     old_path = f"{small_data_dir}{filename}"
+    #     old_path = os.path.abspath(old_path)
+    #     # print(old_path)
+    #     # Image.open(old_path).show()
+        
+    #     new_path = f"{label_dir}/{filename}"
+    #     new_path = os.path.abspath(new_path)
+    #     # print (new_path)
+    #     os.replace(os.path.abspath(old_path), os.path.abspath(new_path))
+
+    #     continue
+
--- src/chess_utils_test.py
+++ src/chess_utils_test.py
@@ -1,6 +1,6 @@
 import pytest
 from collections import deque
-from chess_utils import E, W, B, _translate_fe_row, translate_fe
+from chess_utils import E, W, B, translate_fe_notation, one_hot_wbe, translate_fe_to_int_notation
 
 
 @pytest.mark.parametrize(
@@ -16,24 +16,86 @@ from chess_utils import E, W, B, _translate_fe_row, translate_fe
     ],
 )
 def test_row_8_digits(test_row, expected_res):
-    res = _translate_fe_row(test_row)
+    res = translate_fe_notation(test_row)
 
     assert len(res) == len(expected_res)
     for popped_element, expected_element in zip(res, expected_res):
         assert popped_element == expected_element
 
+@pytest.mark.parametrize(
+        "fe_notation, expected_res",
+        [
+            (
+                "1B1K4-2b5-8-8-R3R2P-5k2-N3R1r1-1r2RR2",
+                [
+                    [E, W, E, W, E, E, E, E],
+                    [E, E, B, E, E, E, E, E],
+                    [E] * 8,
+                    [E] * 8,
+                    [W, E, E, E, W, E, E, W],
+                    [E, E, E, E, E, B, E, E],
+                    [W, E, E, E, W, E, B, E],
+                    [E, B, E, E, W, W, E, E],
+                ]
+            ),
+            ("8-8-8-8-8-8-8-8", [[E] * 8] * 8),
+            ("NNNNNNNN-NNNNNNNN-NNNNNNNN-NNNNNNNN-NNNNNNNN-NNNNNNNN-NNNNNNNN-NNNNNNNN", [[W] * 8] * 8),
+            ("rrrrrrrr-rrrrrrrr-rrrrrrrr-rrrrrrrr-rrrrrrrr-rrrrrrrr-rrrrrrrr-rrrrrrrr", [[B] * 8] * 8),
+            (
+                "8-rrrrrrrr-NNNNNNNN-8-rrrrrrrr-NNNNNNNN-8-rrrrrrrr",
+                [
+                    [E] * 8,
+                    [B] * 8,
+                    [W] * 8,
+                    [E] * 8,
+                    [B] * 8,
+                    [W] * 8,
+                    [E] * 8,
+                    [B] * 8,
+                ]
+            ),
+        ],
+        ids=["full", "empty", "white", "black", "mixed_sequences"],
+)
+def test_entire_fe(fe_notation, expected_res):
+    res = translate_fe_notation(fe_notation=fe_notation)
 
-def test_entire_fe():
+    expected_res = ["".join(row) for row in expected_res]
+    expected_res = "-".join(expected_res)
+    assert expected_res == res
+
+
+def test_fe_to_WBE():
     fe_notation = "1B1K4-2b5-8-8-R3R2P-5k2-N3R1r1-1r2RR2"
-    expected_res = [
-        deque([E, W, E, W, E, E, E, E]),
-        deque([E, E, B, E, E, E, E, E]),
-        deque([E] * 8),
-        deque([E] * 8),
-        deque([W, E, E, E, W, E, E, W]),
-        deque([E, E, E, E, E, B, E, E]),
-        deque([W, E, E, E, W, E, B, E]),
-        deque([E, B, E, E, W, W, E, E]),
-    ]
-    res = translate_fe(fe_notation=fe_notation)
+    expected_res = "EWEWEEEE-EEBEEEEE-EEEEEEEE-EEEEEEEE-WEEEWEEW-EEEEEBEE-WEEEWEBE-EBEEWWEE"
+
+    res = translate_fe_notation(fe_notation=fe_notation)
+    assert expected_res == res
+
+
+def test_WBE_to_ints():
+    def change_2_to_minus1(el):
+        el = int(el)
+        if el == 2:
+            return -1
+        return el
+    WBE_notation = "EWEWEEEE-EEBEEEEE-EEEEEEEE-EEEEEEEE-WEEEWEEW-EEEEEBEE-WEEEWEBE-EBEEWWEE"
+    expected_res = "01010000-00200000-00000000-00000000-10001001-00000200-10001020-02001100".replace(
+        "-", "")
+    expected_res = list(expected_res)
+    expected_res = list(map(change_2_to_minus1, expected_res))
+    print(expected_res)
+    res = translate_fe_to_int_notation(WBE_notation=WBE_notation)
+    assert expected_res == res
+
+
+@pytest.mark.parametrize('int_notation, expected_res', [
+    ([1, 0, -1], ([1, 0, 0], [0, 1, 0], [0, 0, 1])),
+    ([0] * 64, ([0] * 64, [1] * 64, [0] * 64)),
+    ([1] * 64, ([1] * 64, [0] * 64, [0] * 64)),
+    ([-1] * 64, ([0] * 64, [0] * 64, [1] * 64)),
+    ([1, 0, 1, -1, 0, 1], ([1, 0, 1, 0, 0, 1], [0, 1, 0, 0, 1, 0], [0, 0, 0, 1, 0, 0]))
+])
+def test_one_hot_wbe(int_notation, expected_res):
+    res = one_hot_wbe(int_notation=int_notation)
     assert res == expected_res
